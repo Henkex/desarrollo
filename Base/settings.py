1f21f94367268d669015a0363656470340968626
Manuel <manllajaruna@gmail.com>
2018-06-24 12:35:04 -0500

    'Cambiar'

--- Base/settings.py
+++ Base/settings.py
@@ -68,8 +68,12 @@ WSGI_APPLICATION = 'Base.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'djangogirls',
+        'USER': 'name',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': ''
     }
 }
 
