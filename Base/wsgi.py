8927160cbcc6cde78dc6a1b14cb4502bbf27dc4c
Manuel <manllajaruna@gmail.com>
2018-06-24 12:40:06 -0500

    'Actualizar_2'

--- Base/wsgi.py
+++ Base/wsgi.py
@@ -11,7 +11,7 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Real.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Base.settings")
 
 #application = get_wsgi_application()
 from dj_static import Cling
